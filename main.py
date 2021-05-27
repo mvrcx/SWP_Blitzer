76882afef0797bc115c4854d83ac36376cb5b115
Lukas <lukas.kaibel@icloud.com>
2021-05-27 11:23:46 +0200

    Added Folder Categorisation

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import FaceDetection.face_detection as fd
+import cv2
 
 	
 def get_image_urls():
 	filenames = []
-	os.chdir("data")
+	os.chdir("raw_data")
 	for root, directories, files in os.walk("."):
 		for filename in files: 
 			if "jpg" in filename or "png" in filename or "jpeg" in filename:
-				filenames.append("data/"+filename)
+				filenames.append("raw_data/"+filename)
 	os.chdir("..")
 	return filenames
 
@@ -25,8 +26,23 @@ def classify_images():
 		else: 
 			classifications[filename] = 2     # Gruen
 		os.chdir("..")
-	print("\r"+str(int(filenames.index(filename)/len(filenames)*100))+"%")
+	print("\r"+str(100)+"%")
 	return classifications
+	
+def create_folder(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+	return
 
-print(classify_images())
+def move_to_folders(classifications):
+	create_folder("clean_data/Rot")
+	create_folder("clean_data/Gruen")
+	for classification in classifications:
+		if classifications[classification] == 0:
+			os.replace(classification, "clean_data/Rot/"+classification[9:])
+		else:
+			os.replace(classification, "clean_data/Gruen/"+classification[9:])
 
+
+
+move_to_folders(classify_images())
