da6f031324e6dd6e818093b47bd29f077b0d58c7
Lukas <lukas.kaibel@icloud.com>
2021-05-27 18:57:20 +0200

    Added documentation and cleaned up code

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,7 +1,8 @@
 import cv2
 import dlib
-from matplotlib import pyplot as plt
 
+# Takes URL of an image and returns an array of face coordinates for all 
+# detected faces in that image
 
 def get_face_rects(url):
 	# Load the detector
@@ -13,14 +14,19 @@ def get_face_rects(url):
 	# Convert image into grayscale
 	gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
 	
-	# Use detector to find landmarks
+	# Stores the coordinates of the detected faces
 	face_coordinates = []
+	
+	# Rectangles that contain faces
 	rects = detector(gray, 1)
+	
+	# Iterate over face rectangles
 	for (i, rect) in enumerate(rects):
 	    x1 = rect.rect.left()
 	    y1 = rect.rect.top()
 	    x2 = rect.rect.right()
 	    y2 = rect.rect.bottom()
-	    # Rectangle around the face
+	    # Adds upper-left and lower-right corner to the face_coordinates array
 	    face_coordinates.append(((x1, y1), (x2, y2)))
+
 	return face_coordinates
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
 import FaceDetection.face_detection as fd
-import cv2
 
-	
+
+# Constants
+
+raw_data = "raw_data"
+clean_data = "clean_data"
+accepted_image_formats = ["jpg", "png", "jpeg"]
+
+# Returns URL's of all images in the data_raw folder
+
 def get_image_urls():
 	filenames = []
-	os.chdir("raw_data")
+	os.chdir(raw_data)
 	for root, directories, files in os.walk("."):
 		for filename in files: 
-			if "jpg" in filename or "png" in filename or "jpeg" in filename:
-				filenames.append("raw_data/"+filename)
+			if any(image_format in filename for image_format in accepted_image_formats):
+				filenames.append(raw_data+"/"+filename)
 	os.chdir("..")
 	return filenames
+
+# Returns a dictionary that stores the classification 
+# for each URL from get_image_urls
+# 0 (red): no faces detected in image
+# 1 (yellow): face detected, but covered
+# 2 (green): face detected and person identifiable
 
 def classify_images():
 	classifications = {}
@@ -22,26 +35,31 @@ def classify_images():
 		os.chdir("FaceDetection")
 		faces = fd.get_face_rects("../"+filename)
 		if len(faces) == 0:
-			classifications[filename] = 0     # Rot
+			classifications[filename] = 0     
 		else: 
-			classifications[filename] = 2     # Gruen
+			classifications[filename] = 2     
 		os.chdir("..")
 	print("\r"+str(100)+"%")
 	return classifications
-	
+
+# Creates a folder at a path, if it doesn't already exist
+
 def create_folder(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 	return
 
+# Moves the images from raw_data to their specific folder in 
+# data_clean (red, yellow, or green)
+
 def move_to_folders(classifications):
-	create_folder("clean_data/Rot")
-	create_folder("clean_data/Gruen")
+	create_folder(clean_data+"/red")
+	create_folder(clean_data+"/green")
 	for classification in classifications:
 		if classifications[classification] == 0:
-			os.replace(classification, "clean_data/Rot/"+classification[9:])
+			os.replace(classification, clean_data+"/red/"+classification[9:])
 		else:
-			os.replace(classification, "clean_data/Gruen/"+classification[9:])
+			os.replace(classification, clean_data+"/green/"+classification[9:])
 
 
 
