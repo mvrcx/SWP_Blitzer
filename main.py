ea794724fd1c0616e3f39bd37edc23d95fba2e36
Lukas <lukas.kaibel@icloud.com>
2021-06-02 11:22:34 +0200

    Added more documentation

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ import FaceDetection.face_detection as fd
 
 # Constants
 
-raw_data = "raw_data"
-clean_data = "clean_data"
-accepted_image_formats = ["jpg", "png", "jpeg"]
+raw_data = "raw_data"								# directory with images that need to be classified
+clean_data = "clean_data"							# directory that contains the classifications
+accepted_image_formats = ["jpg", "png", "jpeg"]		# file formats that are accepted and loaded as images
+
 
 # Returns URL's of all images in the data_raw folder
 
 def get_image_urls():
 	filenames = []
+	
+	# Move to the raw_data directory, to access the images
 	os.chdir(raw_data)
+	
+	# loop over every file in the current directory (raw_data)
 	for root, directories, files in os.walk("."):
 		for filename in files: 
+			
+			# check if file has accepted format
 			if any(image_format in filename for image_format in accepted_image_formats):
 				filenames.append(raw_data+"/"+filename)
+
+	# change back to original directory
 	os.chdir("..")
 	return filenames
+
 
 # Returns a dictionary that stores the classification 
 # for each URL from get_image_urls
@@ -30,17 +40,23 @@ def classify_images():
 	classifications = {}
 	filenames = get_image_urls()
 	print("Looking for faces...")
+	
+	# iterate through all files from raw_data and analyse for faces 
 	for filename in filenames:
 		print("\r"+str(int(filenames.index(filename)/len(filenames)*100))+"%", end="")
 		os.chdir("FaceDetection")
 		faces = fd.get_face_rects("../"+filename)
+		
+		# Classifiing the image 
 		if len(faces) == 0:
 			classifications[filename] = 0     
 		else: 
 			classifications[filename] = 2     
+
 		os.chdir("..")
 	print("\r"+str(100)+"%")
 	return classifications
+
 
 # Creates a folder at a path, if it doesn't already exist
 
@@ -49,12 +65,15 @@ def create_folder(path):
 		os.makedirs(path)
 	return
 
+
 # Moves the images from raw_data to their specific folder in 
 # data_clean (red, yellow, or green)
 
 def move_to_folders(classifications):
 	create_folder(clean_data+"/red")
 	create_folder(clean_data+"/green")
+	
+	# iterates through all classifications and moves the classified images to the corresponding directories
 	for classification in classifications:
 		if classifications[classification] == 0:
 			os.replace(classification, clean_data+"/red/"+classification[9:])
@@ -63,4 +82,6 @@ def move_to_folders(classifications):
 
 
 
-move_to_folders(classify_images())
+if __name__ == "__main__":
+	move_to_folders(classify_images())
+
