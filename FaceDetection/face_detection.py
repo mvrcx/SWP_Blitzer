c5e5f465cd73bd611a53a7c1a1def1e18977d694
Lukas <lukas.kaibel@icloud.com>
2021-07-14 16:04:58 +0200

    Added Test Images and documentation for code

--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,73 +1,59 @@
 import cv2
 import dlib
-
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
+from typing import List, Tuple
 
 
-# Takes URL of an image and returns an array of face coordinates for all 
-# detected faces in that image
+"""
+This script allows the user to detect faces within a given image, by returning a list of tuples for each 
+face, containing the upper-left and lower-right corner of the rectangle around the face and the confidence
+for the prediction. 
 
-def get_face_rects(url):
+For the face-detection the algorithm uses dlib's CNN Face Detector. 
+More information can be found here: http://dlib.net/cnn_face_detector.py.html .
+"""
+
+
+# -------------------------------------------------------------------------------------------------------- #
+
+def get_face_rects(url: str) -> List[Tuple[Tuple[int, int], Tuple[int, int], float]]:
+    """Gets an image and returns the face-coordinates + the confidence in the prediction
+    
+    Parameters
+    ----------
+    url : str
+        The path of the given image
+    
+    Returns
+    -------
+    face_coordinates : List[Tuple[Tuple[int, int], Tuple[int, int], float]]
+        A list of tuples of the coordinates for each face + the confidence in the prediction of the face 
+    """
+
 	# Load the detector
 	detector = dlib.cnn_face_detection_model_v1("mmod_human_face_detector.dat")
 	
 	# read the image
 	img = cv2.imread(url)
+	img = img[0:int(img.shape[0]/2)]
 	
 	# Convert image into grayscale
 	gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
 	
 	# Stores the coordinates of the detected faces
 	face_coordinates = []
-	
-	# Rectangles that contain faces
-	rects = detector(gray, 1)
+
+    # Detecting faces in gray image
+	faces = detector(gray, 1)
 
 	# Iterate over face rectangles
-	for (i, rect) in enumerate(rects):
-	    x1 = rect.rect.left()
-	    y1 = rect.rect.top()
-	    x2 = rect.rect.right()
-	    y2 = rect.rect.bottom()
+	for i, face in enumerate(faces):
+	    x1 = face.rect.left()
+	    y1 = face.rect.top()
+	    x2 = face.rect.right()
+	    y2 = face.rect.bottom()
 	    # Adds upper-left and lower-right corner to the face_coordinates array
-	    face_coordinates.append(((x1, y1), (x2, y2)))
-	return rects
+	    face_coordinates.append(((x1, y1), (x2, y2), face.confidence))
+	    
+	return face_coordinates
 
-
-
-class Net(nn.Module):
-
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(68, 16)
-        self.fc2 = nn.Linear(16, 16)
-        self.fc3 = nn.Linear(16, 1)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.sigmoid(self.fc1(x))
-        x = self.sigmoid(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-
-def is_covered(landmark_grayscales):
-    model = Net()
-    model.load_state_dict(torch.load("detect_covered_face.pt", map_location=torch.device('cpu')))
-    model.eval()
-    
-    x = np.array(landmark_grayscales)
-    x = torch.from_numpy(x).float()
-    prediction = model(x)
-    
-    if prediction.item() < 0.5:
-        return False
-    else: 
-        return True
-        
-
-
+# -------------------------------------------------------------------------------------------------------- #
--- main.py
+++ main.py
@@ -1,92 +1,165 @@
 import os
 import FaceDetection.face_detection as fd
-from FaceDetection.face_detection import Net
-import FaceDetection.landmark_detection as ld
+from typing import List, Dict
 
 
+"""
+This script allows the user to classify images into 3 categories, based on the visibility of faces inside
+them. The categories are labeled as red, yellow and green. During execution the corresponding folders for 
+each category are created inside the clean_data directory (constant). 
+Images without faces are categorised as red, those with partially visible faces categorised as yellow and 
+fully visible faces are categorised as green. 
+
+To execute, make sure the input and output directories are located at the same path as this main.py file, 
+as well as the FaceDetection directory. 
+
+To controll the threshold for the partially covered faces, change the yellow_folder_threshold in the 
+Constants. A higher threshold corresponds to more images being classified as yellow. 
+
+To see more documentation about the project visit: https://github.com/mvrcx/SWP_Blitzer
+"""
+
+
+# -------------------------------------------------------------------------------------------------------- #
 
 # Constants
 
-raw_data = "input_files"								# directory with images that need to be classified
-clean_data = "output_files"							# directory that contains the classifications
-accepted_image_formats = ["jpg", "png", "jpeg"]		# file formats that are accepted and loaded as images
 
+raw_data = "input_files"							# input directory
+clean_data = "output_files"							# output directory with red, green, yellow folders
 
-# Returns URL's of all images in the data_raw folder
+accepted_image_formats = ["jpg", "png", "jpeg"]		# accepted image formats
 
-def get_image_urls():
-	filenames = []
-	
-	# Move to the raw_data directory, to access the images
-	os.chdir(raw_data)
-	
-	# loop over every file in the current directory (raw_data)
-	for root, directories, files in os.walk("."):
-		for filename in files: 
-			
-			# check if file has accepted format
-			if any(image_format in filename for image_format in accepted_image_formats):
-				filenames.append(raw_data+"/"+filename)
+yellow_folder_threshold = 0.2                       # threshold to determine covered faces (the higher, 
+                                                    # the more images are classified as yellow)
 
-	# change back to original directory
-	os.chdir("..")
-	return filenames
+# -------------------------------------------------------------------------------------------------------- #
 
+def get_image_urls(path: str) -> List[str]:
+    """Returns URL's of all images in the data_raw folder
+    
+    Parameters
+    ----------
+    path : str
+        The location, were the images are located
+        
+    Returns
+    -------
+    filenames : List[str]
+    """
 
-# Returns a dictionary that stores the classification 
-# for each URL from get_image_urls
-# 0 (red): no faces detected in image
-# 1 (yellow): face detected, but covered
-# 2 (green): face detected and person identifiable
+    print("Looking for images in "+raw_data)
+    filenames = []
+    
+    # Move to the raw_data directory, to access the images
+    os.chdir(path)
+    
+    # loop over every file in the current directory (raw_data)
+    for root, directories, files in os.walk("."):
+    	for i, filename in enumerate(files): 
+    		print("\r"+"Collecting filenames... "+str(i+1)+"/"+str(len(files)), end="")
+    		
+    		# check if file has accepted format
+    		if any(image_format in filename for image_format in accepted_image_formats):
+    			filenames.append(raw_data+"/"+filename)
 
+    # change back to original directory
+    os.chdir("..")
+    return filenames
 
-def classify_images():
+# -------------------------------------------------------------------------------------------------------- #
+
+def classify_images() -> Dict[str, int]:
+    """Returns a dictionary that stores the classification for each URL from get_image_urls
+        0 (red): no faces detected in image
+        1 (yellow): face detected, but covered
+        2 (green): face detected and person identifiable
+    
+    Parameters
+    ----------
+    None
+      
+    Returns
+    -------
+    classifications : Dict[str, int]
+        Dictionary containing a classification (0, 1, or 2) for each filename as the key
+    """
+
 	classifications = {}
-	filenames = get_image_urls()
-	print("Looking for faces...")
+	filenames = get_image_urls(raw_data)
+	print("\nBeginning image classification.")
 	
 	# iterate through all files from raw_data and analyse for faces 
-	for filename in filenames:
-		print("\r"+str(int(filenames.index(filename)/len(filenames)*100))+"%", end="")
+	for i, filename in enumerate(filenames):
+		print("\r"+"Classifiing images... "+str(i+1)+"/"+str(len(filenames)), end="")
 		os.chdir("FaceDetection")
 		faces = fd.get_face_rects("../"+filename)
-		#landmark_grayscales = ld.get_gray_scale_of_landmarks_for("../"+filename, faces)
 		
 		# Classifiing the image 
 		if len(faces) == 0:
 			classifications[filename] = 0     
 		else: 
-			classifications[filename] = 2     
+		    for face in faces: 
+		        if face[2] < yellow_folder_threshold:
+		            classifications[filename] = 1
+		            break
+		        else: 
+		            classifications[filename] = 2     
 
 		os.chdir("..")
-	print("\r"+str(100)+"%")
+	print("\nImage classification completed.")
 	return classifications
 
+# -------------------------------------------------------------------------------------------------------- #
 
-# Creates a folder at a path, if it doesn't already exist
+def create_folder(path: str) -> None:
+    """Creates a folder at a path, if it doesn't already exist
+    
+    Parameters
+    ----------
+    path : str
+        The folder path, that should be created
+        
+    Returns
+    -------
+    None
+    """
 
-def create_folder(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
 	return
 
+# -------------------------------------------------------------------------------------------------------- #
 
-# Moves the images from raw_data to their specific folder in 
-# data_clean (red, yellow, or green)
+def move_to_folders(classifications: Dict[str, int]) -> None:
+    """Moves the images from raw_data to their specific folder in data_clean (red, yellow, or green)
+    
+    Parameters
+    ----------
+    classifications : Dict[str, int]
+        Dictionary containing a classification (0, 1, or 2) for each filename as the key 
+        
+    Returns
+    -------
+    None
+    """
 
-def move_to_folders(classifications):
 	create_folder(clean_data+"/red")
+	create_folder(clean_data+"/yellow")
 	create_folder(clean_data+"/green")
 	
 	# iterates through all classifications and moves the classified images to the corresponding directories
 	for classification in classifications:
 		if classifications[classification] == 0:
 			os.replace(classification, clean_data+"/red/"+classification[len(raw_data):])
+		elif classifications[classification] == 1:
+			os.replace(classification, clean_data+"/yellow/"+classification[len(raw_data):])
 		else:
 			os.replace(classification, clean_data+"/green/"+classification[len(raw_data):])
 
-
+# -------------------------------------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
 	move_to_folders(classify_images())
 
+# -------------------------------------------------------------------------------------------------------- #
